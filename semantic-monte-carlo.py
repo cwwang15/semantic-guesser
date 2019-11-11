a199d207da860c1915f2ab44a0d5937530dceb7c
Chuanwang WANG <chaunecy@outlook.com>
2019-11-11 09:25:08 +0800

    调整了一些变量

--- semantic-monte-carlo.py
+++ semantic-monte-carlo.py
@@ -41,6 +41,7 @@ spliter = chr(3)
 def exec_strength(sample_file, scored_password_file, monte_carlo_result_file):
     cmd = "python -m guessing.strength %s %s > %s" \
           % (sample_file, scored_password_file, monte_carlo_result_file)
+    print(cmd)
     result = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE
     )
@@ -91,7 +92,7 @@ def __exec_strength(scored_sample_file, scored_test_file, monte_carlo_result_fil
         except Exception:
             log_prob = float("inf")
         idx = bisect.bisect_right(log_probs, log_prob)
-        fout.write("%s%s%f\n" % (pwd, spliter, positions[idx - 1] if idx > 0 else 0))
+        fout.write("%s%s%f\n" % (pwd, "\t", positions[idx - 1] if idx > 0 else 0))
     fin_scored_test.close()
     fout.flush()
     fout.close()
@@ -121,31 +122,31 @@ def generate_guess_crack(monte_carlo_result_filename, guess_crack_filename):
 
 
 if __name__ == "__main__":
-    _tag = "rockyou-1-255-carck-myspqce-1-255"
+    _tag = "../models/rockyou-combine-01-255/word2vec-to-semantic"
     _abs_dir = sys.path[0]
     _password_file = "/home/chaun/Documents/Passwords/RockYou/rockyou_new.txt"
-    _path_to_grammar = os.path.join(_abs_dir, "models", _tag)
+    _path_to_grammar = os.path.join(_abs_dir, _tag)
     _sample_file = os.path.join(_path_to_grammar, "sample.txt")
-    _test_file = "/home/chaun/Documents/Passwords/MySpace/myspace_new.txt"
+    _test_file = "/home/cw/Documents/Passwords/RockYou/rockyou-1-255/test.txt"
     _scored_test_file = os.path.join(_path_to_grammar, "scored_test.txt")
     _strength_file = os.path.join(_path_to_grammar, "pwd_strength.txt")
     _guess_crack_file = os.path.join(_path_to_grammar, "guess_crack.txt")
     _curve_filename = os.path.join(_path_to_grammar, "guess_crack")
     logging.info("Generating grammar...")
-    generate_grammar(_password_file, _path_to_grammar)
+    # generate_grammar(_password_file, _path_to_grammar)
     logging.info("Generating grammar done")
     logging.info("Generating samples...")
     exec_sample(_path_to_grammar, _sample_file, sample_size=100000)
     logging.info("Generating samples done")
     logging.info("Scoring test set...")
-    exec_score(_path_to_grammar, _test_file, _scored_test_file)
+    # exec_score(_path_to_grammar, _test_file, _scored_test_file)
     logging.info("Scoring test done")
     logging.info("Evaluating strength...")
-    exec_strength(_sample_file, _scored_test_file, _strength_file)
+    __exec_strength(_sample_file, _scored_test_file, _strength_file)
     logging.info("Evaluating strength done")
     logging.info("Generating guess crack pair...")
     generate_guess_crack(_strength_file, _guess_crack_file)
     logger.info("Generating guess crack pair done")
     logger.info("Drawing guess crack curve...")
-    draw_guess_crack_curve(_guess_crack_file, _curve_filename, _tag, _test_file)
+    # draw_guess_crack_curve(_guess_crack_file, _curve_filename, _tag, _test_file)
     logger.info("Drawing guess crack curve done")
