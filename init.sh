4b7792c2f6fef61ab91b683e0a2333c0854397b6
Chuanwang WANG <chaunecy@outlook.com>
2019-12-22 16:58:16 +0800

    fix(init): add nltk downloader cmd

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 python3 -m venv venv && \
     ./venv/bin/pip install -r ./requirements.txt && \
+    ./venv/bin/python -m nltk.downloader wordnet wordnet_ic sentiwordnet && \
     cd ./guessing && make
