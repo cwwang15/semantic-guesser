05984d1e4ac3aac8343e7583566ca7737cdb6596
Chuanwang WANG <chaunecy@outlook.com>
2019-12-22 19:30:14 +0800

    fix(init): fix the bug of download nltk

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-python3 -m venv venv && \
-    ./venv/bin/pip install -r ./requirements.txt && \
-    ./venv/bin/python -m nltk.downloader wordnet wordnet_ic sentiwordnet && \
-    cd ./guessing && make
+#!/usr/bin/env bash
+python3 -m venv venv
+source ./venv/bin/activate
+pip install -r ./requirements.txt
+python -m nltk.downloader wordnet wordnet_ic sentiwordnet
+cd ./guessing && make
