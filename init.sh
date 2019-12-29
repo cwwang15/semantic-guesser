fc70e6fa50cc9a6800c2154251460b09357beb39
Chuanwang WANG <chaunecy@outlook.com>
2019-12-29 19:39:01 +0800

    fix(init): fix the bug of failure to init

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 python3 -m venv venv
-source ./venv/bin/activate
-pip install -r ./requirements.txt
-python -m nltk.downloader wordnet wordnet_ic sentiwordnet
+./venv/bin/pip install -r ./requirements.txt
+./venv/bin/python -m nltk.downloader wordnet wordnet_ic sentiwordnet
 cd ./guessing && make
